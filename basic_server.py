af97529f279c9a20391e563a2f3b0c2f43f576f6
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2024-03-15 13:28:49 +0300

    everything fixed and #nocaching

--- basic_server.py
+++ basic_server.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from pydantic import BaseModel
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 from chat_with_felix import analyze_excerpt
+from fastapi.templating import Jinja2Templates
 import os
-
 
 class CustomHeaderMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
@@ -16,14 +16,11 @@ class CustomHeaderMiddleware(BaseHTTPMiddleware):
         response.headers["Cache-Control"] = "no-store"
         return response
 
-
 app = FastAPI()
-
 
 # Define a Pydantic model for the request body
 class InterviewExcerpt(BaseModel):
     text: str
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -36,15 +33,13 @@ app.add_middleware(
 # Serve static files from the "static" directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Set up Jinja2 templates
+templates = Jinja2Templates(directory="static")
 
-@app.get("/", response_class=FileResponse)
-def get_root():
-    # Use the absolute path to return the HTML file as a response
-    absolute_path_to_html = os.path.join(
-        "/Users/alexanderpodolsky/Documents/InterviewCoPilot/static", "basic_file.html"
-    )
-    return FileResponse(absolute_path_to_html)
-
+@app.get("/", response_class=HTMLResponse)
+async def get_root(request: Request):
+    # Render the HTML template with Jinja2
+    return templates.TemplateResponse("basic_file.html", {"request": request})
 
 @app.post("/analyze-text/")
 def analyze_text(excerpt: InterviewExcerpt):
@@ -54,7 +49,6 @@ def analyze_text(excerpt: InterviewExcerpt):
     print(analysis_result)
     # You return a dictionary because FastAPI automatically converts it to JSON
     return {"analysis": analysis_result}
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
